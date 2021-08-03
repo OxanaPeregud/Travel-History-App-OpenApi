73e1c48d504ee8c4020802e6b1591f78c47541bb
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-08-03 17:16:08 +0300

    initial commit

---
+++ src/main/java/com/peregud/travelhistory/TravelHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.peregud.travelhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peregud/travelhistory/TravelHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peregud.travelhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
